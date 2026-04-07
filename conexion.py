7367353a0e01c138bd4791bee54227d890dc1ee2
lxvyromero-cloud <lxvyromero@gmail.com>
2026-04-07 13:21:21 -0600

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,14 +1,9 @@
 import sqlite3
 
-# ERROR 1: Contraseña expuesta (Hardcoded Secret)
-DB_PASSWORD = "super-secret-password-123" 
-
-def buscar_usuario(nombre_usuario):
-    conexion = sqlite3.connect("usuarios.db")
-    cursor = conexion.cursor()
-
-    # ERROR 2: Inyección SQL (Concatenación directa)
-    query = "SELECT * FROM usuarios WHERE nombre = '" + nombre_usuario + "'"
-
+def consultar_usuario(id_usuario):
+    db = sqlite3.connect("datos.db")
+    cursor = db.cursor()
+    # Esto es una vulnerabilidad de Inyección SQL (concatenación directa)
+    query = "SELECT * FROM usuarios WHERE id = " + id_usuario
     cursor.execute(query)
     return cursor.fetchone()
